feea11af2231e1f77c3f5415623c119cefca02c6
Conlab_MintArin <shud8745@gmail.com>
2024-08-03 12:57:46 +0900

    CHANGE: JDK version upgrade 8 to 11 for using Logback
    Logback 사용으로 인한 JDK Version 11 이상으로 업그레이드
    
    logback 1.5.x 시리즈부터는 최소 JDK 11 버전을 사용해야 함.

---
+++ src/main/java/net/conlabsmedia/apitester/AppRunner.java
@@ -0,0 +1,14 @@
+package net.conlabsmedia.apitester;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppRunner {
+
+    private final static Logger logger = LoggerFactory.getLogger(AppRunner.class);
+
+    public static void main(String[] args) {
+        logger.debug("test");
+    }
+
+}
